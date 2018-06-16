b5b55049c4f7cbe6222aff9f3601b9a5f1190152
leopuleo <leonardo.giacone@gmail.com>
2018-06-16 14:01:26 +0200

    Aggiunto renderToString

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { renderToString } from 'react-dom/server'
+
 import createStore from './src/store'
 
 exports.replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
