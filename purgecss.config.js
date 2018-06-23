41586a9764f8d56e2e1ca748a36ef34f303eb875
leopuleo <leonardo.giacone@gmail.com>
2018-06-23 23:09:07 +0200

    Corretto errore purgecss: file vuoto in categoriesList

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.js'],
-  css: ['./src/layouts/index.css'],
+  css: ['./src/layouts/index.css', './src/layouts/tailwind.css'],
+  fontFace: true,
+  keyframes: true,
   whitelist: ['body', 'html'],
   extractors: [
     {
       extractor: class {
         static extract(content) {
-          return content.match(/[A-z0-9-:\/]+/g) || []
+          return content.match(/[A-z0-9-:\/]+/g)
         }
       },
       extensions: ['html', 'js'],
---
+++ src/components/categoriesList/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CategoriesList = () => (
+  <div className="category-list">Categorie</div>
+)
+
+export default CategoriesList
