ead83a3eb0b76df2fb750b6ddb12e27dbe4df8d7
leopuleo <leonardo.giacone@gmail.com>
2018-06-24 16:28:52 +0200

    Impostata gallery in single portfolio

--- src/components/gallery/index.js
+++ src/components/gallery/index.js
@@ -5,34 +5,44 @@ class Gallery extends Component {
 	render() {
 		const { gallery, title } = this.props
 
+    // Immagini 1, 5, 11, 15, 21, 25 etc...
+    const getCardFormat = (image, i) => {
+      if ((i % 5 === 0 && i !== 0 && i % 10 !== 0) || (i - 1) % 10 === 0) {
+        return {
+          image: image.horizontal,
+          outerWrapperClassName: 'image-wrapper image-wrapper__horizontal w-2/3 p-5',
+          className: 'image image__horizontal',
+        }
+      } else {
+        return {
+          image: image.vertical,
+          outerWrapperClassName: 'image-wrapper image-image__vertical w-1/3 p-5',
+          className: 'image image__vertical',
+        }
+      }
+    }
+
 		return (
       <div className="gallery">
-      	{ gallery.map((image, i) => {
-          // Immagini 1, 5, 11, 15, 21, 25 etc...
-          if((i % 5 === 0 && i !== 0 && i % 10 !== 0) || ((i - 1) % 10 === 0)) {
+        <div className="gallery-grid flex flex-wrap -mx-5 -mx-5">
+        	{ gallery.map((image, i) => {
+
+            const card = getCardFormat(
+              image.gallery_image.localFile.childImageSharp,
+              i
+            )
             return(
               <Img
                 key={ `image-${i}` }
-                sizes={ image.gallery_image.localFile.childImageSharp.horizontal }
+                sizes={ card.image }
                 fadeIn={ true }
-                className="image image-horizontal"
-                outerWrapperClassName="imageWrapper imageWrapper-horizontal"
+                className={ card.className}
+                outerWrapperClassName={ card.outerWrapperClassName}
                 alt={ `${title.text} - ${i}` }
               />
             )
-          } else {
-            return(
-              <Img
-                key={ `image-${i}` }
-                sizes={ image.gallery_image.localFile.childImageSharp.vertical }
-                fadeIn={ true }
-                className="image image-vertical"
-                outerWrapperClassName="imageWrapper imageWrapper-vertical"
-                alt={ `${title.text} - ${i}` }
-              />
-            )
-          }
-        }) }
+          }) }
+        </div>
       </div>
 	  );
 	}
