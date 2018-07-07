47d99c71204c371621861457971a0cfb77a54006
leopuleo <leonardo.giacone@gmail.com>
2018-07-07 15:27:41 +0200

    Rimosso foglio di stile inutile in title

--- src/components/title/index.js
+++ src/components/title/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import './style.css'
-
 const Title = ({ title, cssClasses }) => (
   <header className='page-header w-full mb-6 lg:w-2/3 lg:mb-10'>
     <h1 className={`page-title text-lowercase font-accent-bold text-grey-darkest text-4xl leading-none lg:leading-tight ${cssClasses}`}>
