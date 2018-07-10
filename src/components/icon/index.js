ea3d76be0ffec1dcc1d3b5bf2a2539c4f6085c06
leopuleo <leonardo.giacone@gmail.com>
2018-07-10 19:15:20 +0200

    Inizio lavoro navigazione tra progetti

--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -4,12 +4,13 @@ import PropTypes from 'prop-types'
 import './fontface.js'
 import './style.css'
 
-const Icon = ({ icon }) => (
-  <span className={`icn-${icon}`} />
+const Icon = ({ icon, cssClasses }) => (
+  <span className={`icn-${icon} ${cssClasses}`} />
 )
 
 Icon.propTypes = {
-  icon: PropTypes.string,
+  icon: PropTypes.string.isRequired,
+  cssClasses: PropTypes.string
 }
 
 export default Icon
--- src/components/projectSingle/navigation/index.js
+++ src/components/projectSingle/navigation/index.js
@@ -4,26 +4,48 @@ import Link from 'gatsby-link'
 
 import Icon from '../../icon'
 
+import './style.css'
+
 const ProjectNavigation = ({ next, prev }) => (
-  <div className='project-navigation'>
-    {prev && (
-      <div className='project-prev'>
-        <Link to={prev.uid}>
-          <Icon icon='arrow-slim-left' />
-          <small>Progetto precedente: </small>
-          {prev.data.title.text}
+  <div className='project-navigation fixed w-screen bg-white overflow-x-scroll md:relative md:w-full md:bg-transparent md:mt-10 md:overflow-x-auto'>
+    <div className='project-navigation-inside whitespace-no-wrap overflow-x-auto px-4 pr-8 py-3 lg:whitespace-normal md:px-0 md:py-0 flex'>
+      <div className='project-prev w-1/3 text-left lg:w-1/2'>
+        {prev && (
+          <Link
+            to={prev.uid}
+            className='text-grey-darkest no-underline hover:no-underline text-xl inline-flex'
+          >
+            <Icon icon='arrow-slim-left' cssClasses='text-4xl mr-3' />
+            <span className='font-sans-bold text-lowercase hidden sm:block mt-1'>
+              <small className='font-sans text-grey text-xs block'>Progetto precedente: </small>
+              {prev.data.title.text}
+            </span>
+          </Link>
+        )}
+      </div>
+      <div className='project-archive w-1/3 text-center lg:hidden'>
+        <Link
+          to='/portfolio/'
+          className='text-grey-darkest no-underline hover:no-underline'
+        >
+          <Icon icon='grid-nav' cssClasses='text-5xl' />
         </Link>
       </div>
-    )}
-    {next && (
-      <div className='project-next'>
-        <Link to={next.uid}>
-          <small>Progetto successivo: </small>
-          {next.data.title.text}
-          <Icon icon='arrow-slim-right' />
-        </Link>
+      <div className='project-next w-1/3 text-right lg:w-1/2'>
+        {next && (
+          <Link
+            to={next.uid}
+            className='text-grey-darkest no-underline hover:no-underline text-xl inline-flex'
+          >
+            <span className='font-sans-bold text-lowercase hidden sm:block mt-1'>
+              <small className='font-sans text-grey text-xs block'>Progetto successivo: </small>
+              {next.data.title.text}
+            </span>
+            <Icon icon='arrow-slim-right' cssClasses='text-4xl ml-3' />
+          </Link>
+        )}
       </div>
-    )}
+    </div>
   </div>
 )
 
