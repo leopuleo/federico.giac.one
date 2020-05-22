d37e21d790400a7db126ea3bc59eb61e5b2cd631
Leonardo Giacone <leonardo.giacone@gmail.com>
2020-05-22 13:21:51 +0200

    feat: add Link component

---
+++ src/components/link/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { Link as GatsbyLink } from "gatsby";
+
+const Link = ({ children, to, activeClassName, partiallyActive, ...other }) => {
+  const internal = /^\/(?!\/)/.test(to);
+
+  if (internal) {
+    return (
+      <GatsbyLink
+        to={to}
+        activeClassName={activeClassName}
+        partiallyActive={partiallyActive}
+        {...other}
+      >
+        {children}
+      </GatsbyLink>
+    );
+  }
+  return (
+    <a href={to} {...other}>
+      {children}
+    </a>
+  );
+};
+
+Link.propTypes = {
+  children: PropTypes.node.isRequired,
+  to: PropTypes.string.isRequired,
+  activeClassName: PropTypes.string,
+  partiallyActive: PropTypes.string,
+};
+
+export default Link;
